d791ce9f1a710fd0f671dd583b7401a06209c60c
Maxim Gérard <maxim.gerard@student.uclouvain.be>
2025-02-18 09:51:46 +0100

    refactor: optimize Dijkstra implementation and remove unused functions

--- tests/dijkstra_test.py
+++ tests/dijkstra_test.py
@@ -5,7 +5,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import copy
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from code import utility_functions as uf
 from code import dijkstra as dij
 import time
 
@@ -50,29 +49,32 @@ def dijkstra_tests(num_nodes=10, prob_edge=0.1, num_starts=5, num_ends_per_start
     distances3, paths3 = dij.optimized_dijkstra(graph, starts, copy.deepcopy(endss))
     end_time3 = time.time()
 
+    start_time4 = time.time()
+    distances4, paths4 = dij.dijkstra_adj(nx.to_numpy_array(graph, weight="distance"), starts, copy.deepcopy(endss))
+    end_time4 = time.time()
+
     print(f"Time taken for our Dijkstra's algorithm: {end_time - start_time} seconds")
     print(f"Time taken for NetworkX's Dijkstra's algorithm: {end_time2 - start_time2} seconds")
     print(f"Time taken for optimized Dijkstra's algorithm: {end_time3 - start_time3} seconds")
+    print(f"Time taken for adjacency matrix Dijkstra's algorithm: {end_time4 - start_time4} seconds")
     print("our Dijkstra's algorithm is {} times faster than NetworkX's".format((end_time2 - start_time2)/(end_time - start_time)))
     print("our Dijkstra's algorithm is {} times faster than optimized Dijkstra's".format((end_time3 - start_time3)/(end_time - start_time)))
-    return distances, distances2, distances3, starts, endss, paths, paths3, graph
+    return distances, distances4, starts, endss, paths, paths4, graph
 
 #------#
 #-TEST-#
 #------#
 
-distances, distances2, distances3, starts, endss, paths, paths3, graph = dijkstra_tests(num_nodes=70, prob_edge=0.5, num_starts=5, num_ends_per_start=5)
+distances, distances4, starts, endss, paths, paths4, graph = dijkstra_tests(num_nodes=70, prob_edge=0.5, num_starts=5, num_ends_per_start=5)
 print( "VERIF DES DISTANCES ")
 for i in range(len(starts)): 
     start = starts[i]  
     for j in endss[i]:
-        if j in distances2[start][0]:
-            assert distances[start][j] == distances2[start][0][j], f"Erreur: {distances[start][j]} != {distances2[start][0][j]}"
-            assert distances[start][j] == distances3[start][j], f"Erreur: {distances[start][j]} != {distances3[start][j]}"
+        assert distances[start][j] == distances4[start][j], f"Erreur: {distances[start][j]} != {distances4[start][0][j]}"
         if paths[start][j] != [] and paths[start][j] != None:
-            #print("Distance entre",start," et ",j," : ",distances[start][j])
-            #print("Chemin : ",paths[start][j])
-            #print("CHemin 3 : ",paths3[start][j])
+            print("Distance entre",start," et ",j," : ",distances[start][j])
+            print("Chemin : ",paths[start][j])
+            print("CHemin 4 : ",paths4[start][j])
             pass
 #pos = nx.spring_layout(graph)  # Positionne les nœuds pour une visualisation claire
 #nx.draw(graph, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=200, font_size=5)
