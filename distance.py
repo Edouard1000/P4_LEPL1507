2f6f9919229fca6475e9f75f482bc695b31aac08
Edouard Meurant <edouard@wifi-student2-35.sri.ucl.ac.be>
2025-02-18 10:20:22 +0100

    remove distance.py

--- distance.py
+++
@@ -1,8 +0,0 @@
-import math
-from geographiclib.geodesic import Geodesic
-
-geod = Geodesic.WGS84
-
-def earth_distance(lat1, lon1, lat2, lon2):
-    g = geod.Inverse(lat1, lon1, lat2, lon2)
-    return g['s12'] / 1000  # Conversion de la distance en kilomètres
