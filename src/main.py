c5ece553ae5e21bf6970a9743a1582fa972e4813
Maxim Gérard <maxim.gerard@student.uclouvain.be>
2025-02-25 09:05:46 +0100

    refactor: clean up import statements and adjust output file paths

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
 import os
 import sys
-<<<<<<< HEAD:python_files/main.py
 from parse import parse_airport_data
 import f as f
 import utility_functions as uf
-=======
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './python_files')))
-from src.parse import parse_airport_data
-import src.f as f
-import src.utility_functions as uf
->>>>>>> 49e3f45d220135a3858a6aec4934c7aba03826a0:src/main.py
 import networkx as nx
 import pandas as pd
 
@@ -20,21 +13,21 @@ def main():
     airports_file = "./csv/airports.csv"
     routes_file = "./csv/pre_existing_routes.csv"
 
+    # Dossier de sortie
+    output_folder = "./output_csv"
+    os.makedirs(output_folder, exist_ok=True)
+
     
-    network_graph = parse_airport_data(airports_file, routes_file)
+    network_graph, id_to_index = parse_airport_data(airports_file, routes_file)
     network_graph_adj_list = nx.to_dict_of_lists(network_graph)
     # print(network_graph_adj_list)
     network_graph_adj_matrix = nx.adjacency_matrix(network_graph, weight= "distance").todense()
     df = pd.DataFrame(network_graph_adj_matrix)
-    df.to_csv("output_csv/network_graph_adj_matrix.csv", index=False)
+    df.to_csv("./output_csv/network_graph_adj_matrix.csv", index=False)
     airport_to_connect_list = [[0,1]] # à completer
 
     
     C = 5
-
-    # Dossier de sortie
-    output_folder = "output_csv"
-    os.makedirs(output_folder, exist_ok=True)
 
     # Exécuter la recherche de la meilleure trajectoire
     optimal_trajectory = f.findOptimalTrajectory(network_graph_adj_list, C, output_folder, airport_to_connect_list)
