35e694cbfd702d9c58aa4b6e69eed318f52ca444
Franc Thomas <franc.thomas2004@gmail.com>
2025-04-15 09:29:37 +0200

    on commence les tests de performances et clean un peu le code pour des données précise

--- src/generate_journeys.py
+++ src/generate_journeys.py
@@ -1,29 +1,30 @@
 import parse as prs
 import random
 import csv
+def generate_journeys(numberOfJourneys):
+    csv_folder_path = 'c:/Users/thoma/Desktop/3e/P4/code/P4_LEPL1507/csv' # Change this to the path of the csv folder
 
-numberOfJourneys = 20 # Change this to the number of journeys you want to generate
-csv_folder_path = 'c:/Users/thoma/Desktop/3e/P4/code/P4_LEPL1507/csv' # Change this to the path of the csv folder
+    G, _ = prs.parse_airport_data()
+    airports = []
+    for i in G.nodes:
+        airports.append(G.nodes[i]["ID"])
 
-G, _ = prs.parse_airport_data()
-airports = []
-for i in G.nodes:
-    airports.append(G.nodes[i]["ID"])
+    # print(airports)
 
-# print(airports)
-
-journeys = []
-for _ in range(numberOfJourneys):
-    start = random.choice(airports)
-    end = random.choice(airports)
-    while start == end:  # Ensure start and end are different
+    journeys = []
+    for _ in range(numberOfJourneys):
+        start = random.choice(airports)
         end = random.choice(airports)
-    journeys.append((start, end))
+        while start == end:  # Ensure start and end are different
+            end = random.choice(airports)
+        journeys.append((start, end))
 
-print(journeys)
+    # print(journeys)
 
-with open(f'{csv_folder_path}/wanted_journeys.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['ID_start', 'ID_end'])
-    for journey in journeys:
-        writer.writerow(journey)
+    with open(f'{csv_folder_path}/wanted_journeys.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['ID_start', 'ID_end'])
+        for journey in journeys:
+            writer.writerow(journey)
+
+generate_journeys(100)
